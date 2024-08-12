64f7765ed215134eeed2f159e77d11c2e63fb215
EmimBilek <139300433+EmimBilek@users.noreply.github.com>
2024-08-12 11:34:56 +0700

    tambahin projek async await belajar masak

---
+++ kode/BelajarAsyncAwait/async_await_belajar_masak/MetodeMasak/MetodeMasakSarapan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MetodeMasak
+{
+    public class MetodeMasakSarapan
+    {
+        private Random random;
+
+        public MetodeMasakSarapan()
+        {
+            random = new Random();
+        }
+
+        public void MasakTelor()
+        {
+            Console.WriteLine("Sedang masak telor...");
+            int milidetik = random.Next(6000, 7000);
+            Thread.Sleep(milidetik);
+            Console.WriteLine("Masak telor selesai!");
+        }
+        public void MasakKopi()
+        {
+            Console.WriteLine("Sedang bikin kopi...");
+            int milidetik = random.Next(20000, 21000);
+            Thread.Sleep(milidetik);
+            Console.WriteLine("Bikin kopi selesai!");
+        }
+        public void PanggangRoti()
+        {
+            Console.WriteLine("Sedang panggang roti...");
+            int milidetik = random.Next(9000, 12000);
+            Thread.Sleep(milidetik);
+            Console.WriteLine("Panggang roti selesai!");
+        }
+        public void PotongSayur()
+        {
+            Console.WriteLine("Sedang potong sayur...");
+            int milidetik = random.Next(3000, 5000);
+            Thread.Sleep(milidetik);
+            Console.WriteLine("Potong sayur selesai!");
+        }
+        public void SusunRoti()
+        {
+            Console.WriteLine("Sedang susun roti...");
+            int detik = random.Next(3000, 5000);
+            Thread.Sleep(detik);
+            Console.WriteLine("Susun roti selesai!");
+        }
+        public void TuangKopi()
+        {
+            Console.WriteLine("Sedang tuang kopi...");
+            int detik = random.Next(1000, 2000);
+            Thread.Sleep(detik);
+            Console.WriteLine("Tuang kopi selesai!");
+        }
+    }
+}
---
+++ kode/BelajarAsyncAwait/async_await_belajar_masak/async_await_belajar/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using MetodeMasak;
+
+namespace async_await_belajar
+{
+    class Program
+    {
+        private static MetodeMasakSarapan metod = new MetodeMasakSarapan();
+        static async Task Main(string[] args)
+        {
+            Console.Write("Tekan \"Enter\" untuk mulai memasak");
+            Console.ReadLine();
+            var watch = Stopwatch.StartNew();
+
+            Task bikinRoti = BikinRoti();
+            Task bikinKopi = BikinKopi();
+
+            await Task.WhenAll(bikinRoti, bikinKopi);
+
+            watch.Stop();
+            double detik = Math.Round(Convert.ToDouble(watch.ElapsedMilliseconds) / 1000, 1);
+
+            Console.WriteLine("Sarapan sudah siap");
+            Console.WriteLine("Waktu selesai masak : " + detik + " detik");
+
+            Console.ReadKey();
+        }
+
+        private static async Task BikinRoti()
+        {
+            List<Task> taskMasak = new List<Task>();
+            Task masakTelor = Task.Run(() => metod.MasakTelor());
+            Task potongSayur = Task.Run(() => metod.PotongSayur());
+            Task panggangRoti = Task.Run(() => metod.PanggangRoti());
+
+            taskMasak.Add(masakTelor);
+            taskMasak.Add(potongSayur);
+            taskMasak.Add(panggangRoti);
+
+            Task bikinRoti = Task.WhenAll(taskMasak);
+
+            await bikinRoti;
+
+            Task susunRoti = Task.Run(() =>  metod.SusunRoti());
+
+            await susunRoti;
+        }
+
+        private static async Task BikinKopi()
+        {
+            Task bikinKopi = Task.Run(() => metod.MasakKopi());
+            await bikinKopi;
+            Task tuangKopi = Task.Run(() => metod.TuangKopi());
+            await tuangKopi;
+        }
+    }
+}
